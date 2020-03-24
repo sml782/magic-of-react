1922cd98d0b1ee54beda0b7e1f6d8176464db398
wb-sml406419 <wb-sml406419@alibaba-inc.com>
2020-03-24 11:01:58 +0800

    feat: 添加redux-devtool

--- r-5/src/store/index.ts
+++ r-5/src/store/index.ts
@@ -2,6 +2,7 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import userReducer, { UserReducer, UserState } from './user-reducer';
 // import createSagaMiddleware from 'redux-saga';
+import { composeWithDevTools } from 'redux-devtools-extension';
 // import loginSaga from '../action/login-saga';
 
 export interface ReducerMap {
@@ -15,8 +16,10 @@ export interface RootStateMap {
 // const sagaMiddleware = createSagaMiddleware();
 const store = createStore(
   combineReducers({ user: userReducer } as ReducerMap),
-  // applyMiddleware(sagaMiddleware)
-  applyMiddleware(thunk)
+  composeWithDevTools(
+    // applyMiddleware(sagaMiddleware)
+    applyMiddleware(thunk)
+  )
 );
 
 // sagaMiddleware.run(loginSaga);
