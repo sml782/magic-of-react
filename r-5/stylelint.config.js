d848f03c320f4dc02364e367850b70ecabff1946
wb-sml406419 <wb-sml406419@alibaba-inc.com>
2020-03-24 11:29:49 +0800

    feat: add stylelint

--- r-5/stylelint.config.js
+++ r-5/stylelint.config.js
@@ -1,9 +1,9 @@
-{
-  extends: '@ali/stylelint-config-ali',
+module.exports = {
+  extends: 'stylelint-config-standard',
   rules: {
     'selector-nested-pattern': '',
     'selector-max-id': 1,
     'declaration-block-no-shorthand-property-overrides': null,
     'font-family-no-missing-generic-family-keyword': null,
   },
-}
+};
