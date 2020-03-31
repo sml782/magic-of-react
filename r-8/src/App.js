21efbb8a7733ba5100dfe407cd0990823a43187c
wb-sml406419 <wb-sml406419@alibaba-inc.com>
2020-03-31 18:56:03 +0800

    feat: commit work

--- r-8/src/App.js
+++ r-8/src/App.js
@@ -1,7 +1,7 @@
 // import React from 'react';
 // import ReactDOM from 'react-dom';
 import React from './sreact';
-// import ReactDOM from './sreact/react-dom';
+import ReactDOM from './sreact/react-dom';
 import Component from './sreact/component';
 import logo from './logo.svg';
 import './App.css';
@@ -29,45 +29,51 @@ const Jsx = () => (
     <div className="border">
       <h5>hello</h5>
     </div>
-    <FunctionComponent name="function" />
-    <ClassComponent name="class" />
-    <ClassComponent />
-    <>
+    {/* <FunctionComponent name="function" /> */}
+    {/* <ClassComponent name="class" /> */}
+    {/* <ClassComponent /> */}
+    {/* <>
       <h5>文本1</h5>
       <h5>文本2</h5>
-    </>
+    </> */}
 
-    {[1, 2, 3].map(item => {
+    {/* {[1, 2, 3].map(item => {
       return (
         <div className="border" key={item}>
           <p>{item}</p>
           <p>{item}</p>
         </div>
       );
-    })}
+    })} */}
   </div>
 );
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Jsx />
-    </div>
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //     <Jsx />
+  //   </div>
+  // );
+
+  ReactDOM.render(
+    Jsx(),
+    document.querySelector('#app'),
   );
+  return null;
 }
 
 export default App;
--- r-8/src/sreact/react-dom.js
+++ r-8/src/sreact/react-dom.js
@@ -1,3 +1,13 @@
+import { PLACEMENT } from './const';
+// todo 拓展视频 useState 实现update 以及 DELETIONS
+
+// 下一个子任务
+let nextUnitOfWork = null;
+// work in progreess 工作中的fiber root
+let workInProgressRoot = null;
+
+// 现在的根节点
+let currentRoot = null;
 
 // 更新节点内容
 function updateNode(node, props) {
@@ -14,16 +24,42 @@ function updateNode(node, props) {
   }
 }
 
-// 迭代子元素
-function iterationChild(children, container) {
-  if (!children || !children.length) {
+// 构建子 fiber
+function iterationChild(wipFiber, children) {
+  if (!wipFiber || !children || !children.length) {
     return;
   }
-  children.map((item) => {
-    if (!Array.isArray(item)) {
-      return render(item, container);
+
+  // 存储上一次循环 fiber, 供存储兄弟 fiber
+  let prevFiber = null;
+  children.map((item, index) => {
+    // if (!Array.isArray(item)) {
+    //   return render(item, container);
+    // }
+    // return item.map((childitem) => render(childitem, container));
+    const newChildFiber = {
+      type: item.type,
+      props: { ...item.props },
+      // 真实 dom 节点
+      node: null,
+      // 上一次构建的 fiber, 以供比较
+      base: null,
+      // 父 fiber, 用于回溯
+      parent: wipFiber,
+      // 副作用标识
+      effectTag: PLACEMENT,
     }
-    return item.map((childitem) => render(childitem, container));
+
+    if (!index) {
+      // 第一个子 fiber 放入父 fiber 的 child
+      wipFiber.child = newChildFiber;
+    } else {
+      // 挂载兄弟 fiber
+      prevFiber.sibling = newChildFiber;
+    }
+
+    // 记录本地 fiber
+    prevFiber = newChildFiber;
   });
 }
 
@@ -41,9 +77,9 @@ function updateClassCom(Com, props) {
 }
 
 // 创建节点
-function createNode(vnode) {
-  const { type: nodeType, props, children } = vnode;
-
+function createNode(fiber) {
+  const { type: nodeType, props } = fiber;
+  console.log(fiber);
   let node = null;
 
   switch (nodeType) {
@@ -58,18 +94,18 @@ function createNode(vnode) {
     }
 
     default:
-      if (typeof nodeType === 'function') {
-        console.log(nodeType)
-        // 类组件
-        if (nodeType.prototype && nodeType.prototype.isClassComponent) {
-          node = updateClassCom(nodeType, props);
-          break;
-        }
+      // if (typeof nodeType === 'function') {
+      //   console.log(nodeType)
+      //   // 类组件
+      //   if (nodeType.prototype && nodeType.prototype.isClassComponent) {
+      //     node = updateClassCom(nodeType, props);
+      //     break;
+      //   }
 
-        // 函数组件
-        node = updateFunctionCom(nodeType, props);
-        break;
-      }
+      //   // 函数组件
+      //   node = updateFunctionCom(nodeType, props);
+      //   break;
+      // }
 
       // 元素组件
       node = document.createElement(nodeType);
@@ -77,18 +113,111 @@ function createNode(vnode) {
   }
 
   updateNode(node, props);
-  iterationChild(children, node);
   return node;
 }
+
+// 更新 原生标签 fiber
+function updateHostComponent(fiber) {
+  if (!fiber.node) {
+    fiber.node = createNode(fiber);
+  }
+
+  const { children } = fiber;
+  if (!children) {
+    return;
+  }
+
+  iterationChild(fiber, children);
+}
+
+
+/**
+ * 执行任务单元
+ * 1. 执行当前任务
+ * 2. 指定下一次执行任务
+ */
+function performUnitOfWork(nextFiber) {
+  if (!nextFiber) {
+    return null;
+  }
+
+  /***** 执行任务 *****/
+  console.log(nextFiber);
+  // 判断类型
+  const { type: nodeType } = nextFiber;
+
+  // 处理
+  updateHostComponent(nextFiber);
+
+  /***** 找下一个任务 *****/
+  // 深度优先有子fiber即返回
+  if (nextFiber.child) {
+    return nextFiber.child;
+  }
+
+  // 没有的话看有没有下一个兄弟fiber
+  let nF = nextFiber;
+  while (nF) {
+    // 有兄弟 fiber 直接返回
+    if (nF.sibling) {
+      return nF.sibling;
+    }
+
+    // 没有兄弟 fiber, 回溯到父 fiber, 然后继续寻找父 fiber 的兄弟 fiber
+    nF = nF.parent;
+  }
+
+  // 没有找到的话, 返回空
+  return null;
+}
+
+// 提交任务, 分组
+function commitWorker(fiber) {
+
+}
+
+// 提交总任务, 这里不分组
+function commitRoot() {
+  commitWorker(workInProgressRoot.child);
+
+  // currentRoot = wipRoot;
+  // wipRoot = null;
+}
+
+// 工作
+function workLoop(deadline) {
+  console.log(deadline, deadline.didTimeout, deadline.timeRemaining());
+
+  while (nextUnitOfWork && deadline.timeRemaining() > 1) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+  }
+
+  // TODO: 提交任务
+  if (!nextUnitOfWork && workInProgressRoot) {
+    // commit 一次性提交，这里不可中断
+    commitRoot();
+  }
+
+  // 继续下一次单元任务
+  // window.requestIdleCallback(workLoop);
+}
+
+window.requestIdleCallback(workLoop);
 
 function render(vnode, container, callback) {
   // console.log({ type: vnode.type, vnode, container, callback });
 
-  const node = createNode(vnode);
-  container.appendChild(node);
+  workInProgressRoot = {
+    node: container,
+    props: {},
+    child: vnode,
+    children: [vnode],
+    base: currentRoot
+  };
+  nextUnitOfWork = workInProgressRoot;
 
   if (callback) {
-    callback(node);
+    callback();
   }
 }
 
